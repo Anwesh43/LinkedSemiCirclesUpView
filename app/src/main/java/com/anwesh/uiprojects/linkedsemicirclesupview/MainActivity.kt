43a2959a83fa26189e2ebff7abd843a3ee2f2766
Anwesh43 <anweshthecool0@gmail.com>
2019-01-11 11:36:04 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsemicirclesupview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsemicirclesupview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsemicirclesupview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.semicirclesupview.SemiCirclesUpView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SemiCirclesUpView.create(this)
     }
 }
