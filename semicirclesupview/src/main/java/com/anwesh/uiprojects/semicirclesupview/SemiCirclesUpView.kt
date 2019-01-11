e2748cf39fb0b3edc7dc17c3fd9c97b706bd89e3
Anwesh43 <anweshthecool0@gmail.com>
2019-01-11 11:31:27 +0530

    adding SemiCirclsUpStep class

--- semicirclesupview/src/main/java/com/anwesh/uiprojects/semicirclesupview/SemiCirclesUpView.kt
+++ semicirclesupview/src/main/java/com/anwesh/uiprojects/semicirclesupview/SemiCirclesUpView.kt
@@ -142,7 +142,7 @@ class SemiCirclesUpView(ctx : Context) : View(ctx) {
 
         fun draw(canvas : Canvas, paint : Paint) {
             canvas.drawSCUNode(i, state.scale, paint)
-            next?.draw(canvas, paint)
+            prev?.draw(canvas, paint)
         }
 
         fun update(cb : (Int, Float) -> Unit) {
@@ -165,6 +165,29 @@ class SemiCirclesUpView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class  SemiCirclesUpStep(var i : Int) {
+
+        private var curr : SCUNode = SCUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
