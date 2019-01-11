e7dd129fdafb2733d315b212e8df7902a7125f77
Anwesh43 <anweshthecool0@gmail.com>
2019-01-11 10:29:04 +0530

    adding libraries and constants

--- semicirclesupview/src/main/java/com/anwesh/uiprojects/semicirclesupview/SemiCirclesUpView.kt
+++ semicirclesupview/src/main/java/com/anwesh/uiprojects/semicirclesupview/SemiCirclesUpView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.semicirclesupview
 /**
  * Created by anweshmishra on 11/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val semiCircles : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.7f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
